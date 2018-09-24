d2ba7631897e0b72f021723aff7eeea7c7a57621
Steven Eardley <steve@eardley.xyz>
2018-09-24 16:30:52 +0100

    Getting redis to run under supervisor, removing docker from the install process (on test so far) https://github.com/DOAJ/doajPM/issues/1519

--- deploy/deploy-redis.sh
+++ deploy/deploy-redis.sh
@@ -5,25 +5,13 @@ THIS_SCRIPT=`basename "$0"`
 ENV=$1
 
 sudo apt-get update -q -y
-sudo apt-get -q -y install curl linux-image-extra-$(uname -r) linux-image-extra-virtual apt-transport-https ca-certificates redis-tools
-curl -fsSL https://yum.dockerproject.org/gpg | sudo apt-key add -
-# TODO could use apt-key fingerprint to verify the key had fingerprint 58118E89F3A912897C070ADBF76221572C52609D
-sudo add-apt-repository "deb https://apt.dockerproject.org/repo/ ubuntu-$(lsb_release -cs) main"
-sudo apt-get update -q -y
-sudo apt-get -q -y install docker-engine=1.12.3-0~trusty
-echo
-echo "Docker version should be displayed below if successfully installed"
-docker -v
-# might have to be done manually - script terminates at some point after the echoes below, either after adding the group, adding *to* the group, or the newgrp command
-echo
-echo "Setting up Docker to be used without sudo"
-sudo groupadd docker
-sudo gpasswd -a ${USER} docker
-#newgrp docker  # TODO automate this. Calling newgrp will start a subshell and not normally execute commands after this line. If we need to run commands under a different group (as we do - everything below this line), then use redirection, see http://unix.stackexchange.com/q/18897/160468 .
-echo
-echo "Installing Docker Compose, version information should be displayed below"
-curl -L https://github.com/docker/compose/releases/download/1.9.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null && sudo chmod a+x /usr/local/bin/docker-compose
-docker-compose --version
+sudo apt-get -q -y install redis-server=2:2.8.4-2
+echo "Configuring redis to use host $REDIS_BIND_HOST and port $HUEY_REDIS_PORT"
+sudo sed -i "s/port 6379/port $HUEY_REDIS_PORT/g" /etc/redis/redis.conf
+sudo sed -i "s/bind 127.0.0.1/$REDIS_BIND_HOST/g" /etc/redis/redis.conf
+
+echo "Configuring redis to run under supervisor"
+sudo sed -i "s/daemonize.*$/daemonize no/g" /etc/redis/redis.conf
 
 # Restart redis
 /home/cloo/repl/$ENV/doaj/src/doaj/deploy/restart-redis.sh $ENV
--- deploy/restart-redis.sh
+++ deploy/restart-redis.sh
@@ -5,10 +5,4 @@ THIS_SCRIPT=`basename "$0"`
 
 ENV=$1
 
-cd /home/cloo/repl/$ENV/doaj/src/doaj/docker
-docker-compose down || true  # it's fine to fail to bring down redis if it is not started yet
-# for the line below:
-# --build to pick up docker/redis/Dockerfile changes
-# --remove-orphans to remove all containers that are not defined in the docker/docker-compose.yml (allows us to change service names and configuration just by editing that file and deploying
-# -d for detached mode rather than run in foreground
-docker-compose up --build --remove-orphans -d
+sudo supervisorctl restart redis
